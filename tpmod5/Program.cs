a22e92664ad6b27ba835f5c01f4dc0b190dcaf33
Dafa24 <53173575+Dafa24@users.noreply.github.com>
2024-03-18 04:13:43 +0700

    Second Commit

--- tpmod5/Program.cs
+++ tpmod5/Program.cs
@@ -8,8 +8,29 @@ namespace tpmod5
 {
     internal class Program
     {
+        class DataGeneric<T>
+        {
+            T Data { get; set; }
+            public DataGeneric(T data)
+            {
+                Data = data;
+            }
+            public void PrintData()
+            {
+                Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+            }
+        }
+        public class HaloGeneric
+        {
+            public void SapaUser<T>(T userName)
+            {
+                Console.WriteLine($"Halo user {userName}");
+            }
+        }
+
         static void Main(string[] args)
         {
+
         }
     }
 }
